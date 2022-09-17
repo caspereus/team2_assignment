727999b0dc9416f95de9baaa17b05734f3a9d332
I Putu Aldy <putu.aldy@ruangguru.com>
2022-09-17 12:24:33 +0700

    chore(team_assignment_1): auto redirect to video url

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VideoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('video.index');
 });
 
 Route::resource('/video',VideoController::class);
